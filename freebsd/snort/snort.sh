d5447caa03a6ca53ed345105604d03c8bd0c7074
jbedette <john.bedette45@gmail.com>
2024-08-02 15:07:13 -0700

    update firewall script

--- freebsd/snort/snort.sh
+++ freebsd/snort/snort.sh
@@ -31,9 +31,12 @@ SMBGHOST_ALERT='alert tcp any any -> any 445
     msg:"SMBGhost attempt detected";
     flow:to_server,established;
     content:"|FC53 4AAF|";
-    offset:4; depth:4;
-    byte_test:1,!&,0x01,0; byte_test:1,&,0x08,4;
-    metadata:service smb; reference:cve,2020-0796;
+    offset:4;
+    depth:4;
+    byte_test:1,!&,0x01,0;
+    byte_test:1,&,0x08,4;
+    metadata:service smb;
+    reference:cve,2020-0796;
     sid:1000001;
     rev:1;
 )
@@ -41,17 +44,20 @@ SMBGHOST_ALERT='alert tcp any any -> any 445
 SMBGHOST_DROP='drop tcp any any -> any 445 
 (
     msg:"SMBGhost attempt detected - dropping"; 
-    flow:to_server,established; content:"|FC53 4AAF|";
+    flow:to_server,established; 
+    content:"|FC53 4AAF|";
     offset:4;
     depth:4;
-    byte_test:1,!&,0x01,0; byte_test:1,&,0x08,4;
-    metadata:service smb; reference:cve,2020-0796;
+    byte_test:1,!&,0x01,0;
+    byte_test:1,&,0x08,4;
+    metadata:service smb; 
+    reference:cve,2020-0796;
     sid:1000002;
     rev:1;
 )'
 mkdir /usr/local/etc/snort/rules
 echo $SMBGHOST_ALERT > $SMBGHOST_RULES
-echo $SMBGHOST_DROP >> $SMBGHOST_RULES
+# echo $SMBGHOST_DROP >> $SMBGHOST_RULES
 echo ""
 echo "SMB GHOST rule at $SMBGHOST_RULES"
 cat $SMBGHOST_RULES
