d215404bccc19f3075b07290e6dfcf5e44c89e18
jbedette <john.bedette45@gmail.com>
2024-08-02 12:05:55 -0700

    update firewall script

--- freebsd/snort/snort.sh
+++ freebsd/snort/snort.sh
@@ -25,6 +25,8 @@ else
 fi
 
 # Create rule
+SMBGHOST_RULES='/usr/local/etc/snort/rules/smbghost.rules'
 SMBGHOST_ALERT='alert tcp any any -> any 445 (msg:"SMBGhost attempt detected"; flow:to_server,established; content:"|FC53 4AAF|"; offset:4; depth:4; byte_test:1,!&,0x01,0; byte_test:1,&,0x08,4; metadata:service smb; reference:cve,2020-0796; sid:1000001; rev:1;)'
-echo $SMBGHOST_ALERT > /usr/local/etc/snort/rules/smbghost.rules
-
+echo $SMBGHOST_ALERT > $SMBGHOST_RULES
+echo ""
+cat $SMBGHOST_RULES
