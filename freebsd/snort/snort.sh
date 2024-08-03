ab3538591fd39e877c72276bdcab16df40cc2398
jbedette <john.bedette45@gmail.com>
2024-08-03 12:42:12 -0700

    update firewall script

--- freebsd/snort/snort.sh
+++ freebsd/snort/snort.sh
@@ -16,6 +16,10 @@ fi
 echo "Enabling Snort to start on boot..."
 sysrc snort_enable="YES"
 
+# move snort service to correct place
+chmod +x ~/devopsScripts/freebsd/snort/snort3
+cp ~/devopsScripts/freebsd/snort/snort3 /usr/local/etc/rc.d/.
+
 # Check if enabling Snort was successful
 if [ $? -eq 0 ]; then
     echo "Snort is set to start on boot."
@@ -24,7 +28,7 @@ else
     exit 1
 fi
 
-# Create rule
+# Create rules
 SMBGHOST_RULES='/usr/local/etc/snort/rules/smbghost.rules'
 SMBGHOST_ALERT='alert tcp any any -> any 445 
 (
@@ -39,19 +43,6 @@ SMBGHOST_ALERT='alert tcp any any -> any 445
     rev:1;
 )
 '
-# SMBGHOST_ALERT='alert tcp any any -> any 445 
-# (
-#     msg:"SMBGhost attempt detected";
-#     flow:to_server,established;
-#     content:"|FC53 4AAF|";
-#     byte_test:1,!&,0x01,0;
-#     byte_test:1,&,0x08,4;
-#     metadata:service smb;
-#     reference:cve,2020-0796;
-#     sid:1000001;
-#     rev:1;
-# )
-# '
 SMBGHOST_DROP='drop tcp any any -> any 445 
 (
     msg:"SMBGhost attempt detected - dropping"; 
@@ -66,28 +57,28 @@ SMBGHOST_DROP='drop tcp any any -> any 445
 )'
 mkdir /usr/local/etc/snort/rules
 echo $SMBGHOST_ALERT > $SMBGHOST_RULES
-# echo $SMBGHOST_DROP >> $SMBGHOST_RULES
+echo $SMBGHOST_DROP >> $SMBGHOST_RULES
 echo ""
 echo "SMB GHOST rule at $SMBGHOST_RULES"
 cat $SMBGHOST_RULES
 echo ""
+
+# update snort.lua with new file pointing to my rules
 cp ~/devopsScripts/freebsd/snort/snort.lua /usr/local/etc/snort/snort.lua
 
-echo "snort_enable="YES" >> /etc/rc.conf"
-chmod +x ~/devopsScripts/freebsd/snort/snort3
-cp ~/devopsScripts/freebsd/snort/snort3 /usr/local/etc/rc.d/.
 
 
 
+# # Snort rule config tests
 # snort -c /usr/local/etc/snort/snort.lua -T
-echo ""
-snort -c /usr/local/etc/snort/snort.lua -T | grep smbghost
+# echo ""
+# snort -c /usr/local/etc/snort/snort.lua -T | grep smbghost
+# echo ""
 
 echo '#!/bin/sh 
 snort -c /usr/local/etc/snort/snort.lua -T' > snort_check.sh
 chmod +x snort_check.sh
 cp snort_check.sh /usr/local/etc/snort/
-# cp snort_check.sh ~/devopsScripts
 
 service snort3 start
 
