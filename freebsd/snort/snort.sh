5d927bd7b1953f6acdbc235e75eeb49e09c15119
jbedette <john.bedette45@gmail.com>
2024-08-04 12:39:23 -0700

    update firewall script

--- freebsd/snort/snort.sh
+++ freebsd/snort/snort.sh
@@ -86,15 +86,12 @@ chmod -R 755 /var/log/snort
 # echo ""
 
 echo '#!/bin/sh 
-snort -c /usr/local/etc/snort/snort.lua -i hn0 -T' > snort_check.sh
+snort -c /usr/local/etc/snort/snort.lua -R /usr/local/etc/snort/snort.lua -r ~/devopsScripts/SMBGHOST/SMBGhost.pcap' > snort_check.sh
 chmod +x snort_check.sh
-
 
 echo '#!/bin/sh 
 snort -c /usr/local/etc/snort/snort.lua -i hn0 -v' > snort_run.sh
 chmod +x snort_run.sh
-
-
 
 echo "snort3 start"
 service snort3 start
