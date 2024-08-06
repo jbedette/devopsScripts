c32728dd0acef8e31d0086312c08ee69e2a12757
jbedette <john.bedette45@gmail.com>
2024-08-06 13:57:14 -0700

    update firewall script

--- freebsd/snort/snort.sh
+++ freebsd/snort/snort.sh
@@ -97,10 +97,10 @@ ifconfig hn0 -txcsum -rxcsum
 ifconfig hn1 -txcsum -rxcsum
 
 # make a bridge
-ifconfig bridge0 create
-ifconfig bridge1 create
-ifconfig bridge0 addm hn0
-ifconfig bridge1 addm hn1
+# ifconfig bridge0 create
+# ifconfig bridge1 create
+# ifconfig bridge0 addm hn0
+# ifconfig bridge1 addm hn1
 # ifconfig bridge0 addm hn0
 
 
