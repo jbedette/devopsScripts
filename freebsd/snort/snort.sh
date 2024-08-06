84d9a61f82eac62285938f347159cea26ce07b32
jbedette <john.bedette45@gmail.com>
2024-08-06 11:23:00 -0700

    update firewall script

--- freebsd/snort/snort.sh
+++ freebsd/snort/snort.sh
@@ -61,7 +61,7 @@ SMBGHOST_DROP='drop tcp any any -> any 445
     rev:1;
 )'
 SSH_LOG='log tcp any any -> $HOME_NET 22222 (msg:"LOG SSH connection attempt"; sid:1000003; rev:1;)'
-SSH_PASS='pass tcp any any -> $HOME_NET 22222 (msg:"PASS SSH connection attempt"; sid:1000004; rev:1;)'
+SSH_PASS='pass tcp any 22222 -> any 22 (msg:"PASS SSH connection attempt"; sid:1000004; rev:1;)'
 SSH_ALERT='alert tcp any any -> $HOME_NET 22 (msg:"ALERT SSH connection attempt"; sid:1000005; rev:1;)'
 SSH_ALERT_22222='alert tcp any any -> $HOME_NET 22222 (msg:"ALERT SSH connection attempt"; sid:1000006; rev:1;)'
 ANY_ALERT='alert tcp any any -> any any (msg:"any tcp thing happened";sid:1000006;rev:1)'
@@ -69,7 +69,7 @@ ANY_ALERT='alert tcp any any -> any any (msg:"any tcp thing happened";sid:100000
 mkdir /usr/local/etc/snort/rules
 echo $SMBGHOST_ALERT > $SMBGHOST_RULES
 echo $SMBGHOST_DROP >> $SMBGHOST_RULES
-echo $SSH_LOG >> $SMBGHOST_RULES
+# echo $SSH_LOG >> $SMBGHOST_RULES
 echo $SSH_PASS >> $SMBGHOST_RULES
 echo $SSH_ALERT >> $SMBGHOST_RULES
 echo $SSH_ALERT_22222 >> $SMBGHOST_RULES
