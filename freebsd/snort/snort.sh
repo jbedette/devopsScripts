8a12b77ab376c0ed4af491971b9780f3c4f498c1
jbedette <john.bedette45@gmail.com>
2024-08-07 13:00:01 -0700

    trimming snort.sh 5

--- freebsd/snort/snort.sh
+++ freebsd/snort/snort.sh
@@ -13,14 +13,6 @@ sysrc snort_enable="YES"
 chmod +x ~/devopsScripts/freebsd/snort/snort
 cp ~/devopsScripts/freebsd/snort/snort /usr/local/etc/rc.d/.
 
-# Check if enabling Snort was successful
-if [ $? -eq 0 ]; then
-    echo "Snort is set to start on boot."
-else
-    echo "Failed to set Snort to start on boot. Exiting."
-    exit 1
-fi
-
 # Create rules
 SMBGHOST_RULES='/usr/local/etc/snort/rules/smbghost.rules'
 mkdir /usr/local/etc/snort/rules
@@ -40,16 +32,16 @@ chown root:wheel /usr/local/etc/snort/snort.lua
 chown root:wheel /var/log/snort
 
 # turn off sums for testing
-ifconfig hn0 -txcsum -rxcsum
-ifconfig hn1 -txcsum -rxcsum
+# ifconfig hn0 -txcsum -rxcsum
+# ifconfig hn1 -txcsum -rxcsum
 
-echo '#!/bin/sh 
-snort -c /usr/local/etc/snort/snort.lua -R /usr/local/etc/snort/rules/smbghost.rules -r ~/devopsScripts/SMBGHOST/SMBGhost.pcap' -l /var/log/snort > snort_check.sh
-chmod +x snort_check.sh
+# echo '#!/bin/sh 
+# snort -c /usr/local/etc/snort/snort.lua -R /usr/local/etc/snort/rules/smbghost.rules -r ~/devopsScripts/SMBGHOST/SMBGhost.pcap' -l /var/log/snort > snort_check.sh
+# chmod +x snort_check.sh
 
-echo '#!/bin/sh 
-snort -c /usr/local/etc/snort/snort.lua -r ~/devopsScripts/SMBGHOST/SMBGhost.pcap -i hn0 -l /var/log/snort -u root -g wheel' > run_snort.sh
-chmod +x run_snort.sh
+# echo '#!/bin/sh 
+# snort -c /usr/local/etc/snort/snort.lua -r ~/devopsScripts/SMBGHOST/SMBGhost.pcap -i hn0 -l /var/log/snort -u root -g wheel' > run_snort.sh
+# chmod +x run_snort.sh
 
 echo '#!/bin/sh 
 tcpreplay -i hn0 ~/devopsScripts/SMBGHOST/SMBGhost.pcap' > replay_smb.sh
