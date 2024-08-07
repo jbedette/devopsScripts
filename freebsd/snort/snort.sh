7022cdd9d6d479ae41096c6727e16928f99f8a87
jbedette <john.bedette45@gmail.com>
2024-08-07 11:00:59 -0700

    update firewall script

--- freebsd/snort/snort.sh
+++ freebsd/snort/snort.sh
@@ -59,7 +59,7 @@ SMBGHOST_ALERT2='alert tcp any any -> any 445
     sid:1000003;
     rev:1;
 )'
-SMBGHOST_ALERT3='alert tcp any any -> any 445 (msg:"SMBv3 CVE-2020-0796 basic content detection"; flow:to_server,established; content:"|FE|SMB|"; metadata:service netbios-ssn; reference:cve,2020-0796; classtype:attempted-admin; sid:1000010; rev:1;)'
+SMBGHOST_ALERT3='alert tcp any any -> any 445 (msg:"SMBv3 CVE-2020-0796 basic content detection"; content:"|FE|SMB|"; metadata:service netbios-ssn; reference:cve,2020-0796; classtype:attempted-admin; sid:1000010; rev:1;)'
 
 
 SMBGHOST_DROP='drop tcp any any -> any 445 
