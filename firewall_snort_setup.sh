cd9b66609f72ba2f67a8f2cc433dcef74cafbdfe
jbedette <john.bedette45@gmail.com>
2024-08-08 08:50:14 -0700

    firewall fixing

--- firewall_snort_setup.sh
+++ firewall_snort_setup.sh
@@ -3,5 +3,7 @@
 #pkg install -y tcpreplay
 chmod +x ./freebsd/firewall/firewall.sh
 chmod +x ./freebsd/snort/snort.sh
-./freebsd/firewall/firewall.sh
-./freebsd/snort/snort.sh
+chmod +x ./freebsd/firewall/firewall.sh ./freebsd/firewall/firewall_x.sh
+chmod +x ./freebsd/snort/snort.sh ./freebsd/snort/snort_x.sh
+./freebsd/firewall/firewall_x.sh
+# ./freebsd/snort/snort_x.sh
